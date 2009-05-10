6f3bed95e447e537bbbfd831ec5b9d8496b727a7
Caleb Clausen <coatl@inforadical.net>
2009-05-10 12:58:29 -0700

    updated this test input to use utf8 in many more contexts

--- test/data/heart.rb
+++ test/data/heart.rb
@@ -4,6 +4,47 @@ def ♥(name)
 end
 
 ♥ ARGV.shift
-#p ?♥ #not supported yet
+#p ?♥ #not til 1.9
 p :♥
 #♥
+
+def spade; :sam end
+def club; :sangwich end
+
+=begin ♥
+♥♥♥
+ ♥
+=end ♥
+
+alias heart ♥
+alias :heart2 :♥
+undef ♥
+alias ♥ spade
+undef :♥
+alias :♥ :club
+
+♥ 2
+
+$♥=2
+ 
+class A♥
+  @@♥=3
+  def new
+    @♥=4
+  end
+
+  def show
+    ♥=4
+    p $♥,@@♥,@♥,♥
+  end
+end
+
+A♥.new.show
+
+alias $heart $♥
+
+p $heart
+
+p '♥'
+p %w[♥ b c♥ d ♥e f♥g]
+
