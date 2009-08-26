9bef767ad29a6996e11d140ac45ec11c7dd0a7d0
Caleb Clausen <coatl@inforadical.net>
2009-08-25 21:25:21 -0700

    treat ; within goalposts in 1.9 mode (mostly) like a comma

--- lib/rubylexer.rb
+++ lib/rubylexer.rb
@@ -629,6 +629,7 @@ private
      assignment_coming=case nc=nextchar
        when ?=;  not /^=[>=~]$/===readahead(2)
        when ?,; comma_in_lvalue_list? 
+       when (?; if @rubyversion>=1.9); BlockParamListLhsContext===@parsestack.last
        when ?); last_context_not_implicit.lhs
        when ?i; /^in(?!#@@LETTER_DIGIT)/o===readahead(3) and 
                   ForSMContext===last_context_not_implicit
