69db7a5066c37186c5ca751b87b89535047c5396
Caleb Clausen <coatl@inforadical.net>
2009-08-28 20:39:07 -0700

    added some more stabby proc tests

--- test/code/test_1.9.rb
+++ test/code/test_1.9.rb
@@ -3,8 +3,40 @@ require "rubylexer"
 
 class Ruby1_9Tests < Test::Unit::TestCase
   def test_stabby_roughly
-    tokens=RubyLexer.new('string','->a; h do 123 end').to_a
+    tokens=RubyLexer.new('string','->a; h do 123 end',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
 
-    assert_equal tokens.grep(RubyLexer::MethNameToken),[]
+    tokens=RubyLexer.new('string','->{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+
+    tokens=RubyLexer.new('string','-> {}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+
+    tokens=RubyLexer.new('string','->;{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+
+    tokens=RubyLexer.new('string','->a{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+
+    tokens=RubyLexer.new('string','->a {}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+
+    tokens=RubyLexer.new('string','->a;{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+
+    tokens=RubyLexer.new('string','->a,b{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+
+    tokens=RubyLexer.new('string','->a,b;{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+
+    tokens=RubyLexer.new('string','->a,b;c{}',1,0,:rubyversion=>1.9).to_a
+    assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+    assert_equal [],tokens.grep(RubyLexer::MethNameToken)
   end
 end
