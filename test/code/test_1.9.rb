eeab8d1d3ea68e3aa5da27e965974d10a5399b7f
Caleb Clausen <coatl@inforadical.net>
2009-09-23 18:51:12 -0700

    when exception happens during these tests, print the guilty snippet

--- test/code/test_1.9.rb
+++ test/code/test_1.9.rb
@@ -21,9 +21,13 @@ class Ruby1_9Tests < Test::Unit::TestCase
 
   def test_stabby_roughly
     EXPECT_NO_METHODS.each{|snippet| 
-      tokens=RubyLexer.new('string',snippet,1,0,:rubyversion=>1.9).to_a
-      assert_equal [],tokens.grep(RubyLexer::MethNameToken)
-      assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+      begin
+        tokens=RubyLexer.new('string',snippet,1,0,:rubyversion=>1.9).to_a
+        assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+        assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+      rescue Exception=>e
+        raise e.class.new(e.message+" while testing '#{snippet}'")
+      end
     }
   end
 end
