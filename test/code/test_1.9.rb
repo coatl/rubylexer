6b0e1f1d645149b0274d62e83f7ed30643c0a50f
Caleb Clausen <coatl@inforadical.net>
2009-09-23 20:46:38 -0700

    added some 1.9 test snippets which should contain only 1 method token

--- test/code/test_1.9.rb
+++ test/code/test_1.9.rb
@@ -20,11 +20,29 @@ class Ruby1_9Tests < Test::Unit::TestCase
     '$f.($x,$y)',
   ]
 
+  EXPECT_1_METHOD=[
+    'def __ENCODING__.foo; 1 end',
+    'def __FILE__.foo; 1 end',
+    'def __LINE__.foo; 1 end',
+    'def self.foo; 1 end',
+    '->{ foo=1 }; foo',
+    '->do foo=1 end; foo',
+  ]
+
   def test_stabby_roughly
     EXPECT_NO_METHODS.each{|snippet| 
       begin
         tokens=RubyLexer.new('string',snippet,1,0,:rubyversion=>1.9).to_a
         assert_equal [],tokens.grep(RubyLexer::MethNameToken)
+        assert_equal [],tokens.grep(RubyLexer::ErrorToken)
+      rescue Exception=>e
+        raise e.class.new(e.message+" while testing '#{snippet}'")
+      end
+    }
+    EXPECT_1_METHODS.each{|snippet| 
+      begin
+        tokens=RubyLexer.new('string',snippet,1,0,:rubyversion=>1.9).to_a
+        assert_equal 1,tokens.grep(RubyLexer::MethNameToken).size
         assert_equal [],tokens.grep(RubyLexer::ErrorToken)
       rescue Exception=>e
         raise e.class.new(e.message+" while testing '#{snippet}'")
