960ae6d68229676f979da6a0aecdb4be967b6e7b
Caleb Clausen <coatl@inforadical.net>
2009-11-02 08:47:00 -0800

    move 1.9 test data into separate file, so it can be reused

--- test/code/test_1.9.rb
+++ test/code/test_1.9.rb
@@ -1,55 +1,9 @@
 require 'test/unit'
 require "rubylexer"
+require "rubylexer/test/oneliners_1.9"
 
 class Ruby1_9Tests < Test::Unit::TestCase
-  EXPECT_NO_METHODS=[ #no errors either
-    '->a; h do 123 end',
-    '->{}',
-    '-> {}',
-    '->;{}',
-    '->(;){}',
-    '->a{}',
-    '->a {}',
-    '->a;{}',
-    '->(a;){}',
-    '->a,b{}',
-    '->a,b;{}',
-    '->a,b;c{}',
-    '->(a,b;){}',
-    '->(a,b;c){}',
-    '$f.($x,$y)',
-    '$f::($x,$y)',
-    '__ENCODING__',
-    'module __ENCODING__::A; end',
-  ]
-
-  EXPECT_1_METHOD=[
-    'def self.foo; 1 end',
-    '->{ foo=1 }; foo',
-    '->do foo=1 end; foo',
-    'def __ENCODING__; 342 end',
-    'def __ENCODING__.foo; 1 end',
-    'def __FILE__.foo; 1 end',
-    'def __LINE__.foo; 1 end',
-    'def a(b,*c,d) 1 end',
-    'def a(*c,d) 1 end',
-    'a{|b,*c,d| 1 }',
-    'a{|*c,d| 1 }',
-    'def a(b,(x,y),d) 1 end',
-    'def a((x,y),d) 1 end',
-    'a{|b,(x,y),d| 1 }',
-    'a{|(x,y),d| 1 }',
-    'def a(b,(x,*y),d) 1 end',
-    'def a((x,*y),d) 1 end',
-    'a{|b,(x,*y),d| 1 }',
-    'a{|(x,*y),d| 1 }',
-    'def a(b,(x,(y,z)),d) 1 end',
-    'def a((x,(y,z)),d) 1 end',
-    'a{|b,(x,(y,z)),d| 1 }',
-    'a{|(x,(y,z)),d| 1 }',
-    'module __ENCODING__::A include B; end',
-  ]
-
+  include Ruby1_9OneLiners
   def test_1_9_roughly
     EXPECT_NO_METHODS.each{|snippet| 
       begin
