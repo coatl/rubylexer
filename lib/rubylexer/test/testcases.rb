28252cfb288786ab3cd508dc6c55cb72d40d959e
Caleb Clausen <caleb@inforadical.net>
2011-01-09 19:32:57 -0800

    ignore blank lines in oneliners.rb and illegal_oneliners.rb

--- lib/rubylexer/test/testcases.rb
+++ lib/rubylexer/test/testcases.rb
@@ -1,10 +1,10 @@
 module TestCases
 #  fail unless File.exist 'test/data/oneliners.rb' and  File.exist 'test/data/stanzas.rb'
   rldir=$:.find{|dir| File.exist? dir+'/rubylexer/test/oneliners.rb' and  File.exist? dir+'/rubylexer/test/stanzas.rb' }
-  ONELINERS=IO.readlines(rldir+'/rubylexer/test/oneliners.rb').map{|x| x.chomp}.grep(/\A\s*[^#\s\n]/).reverse
+  ONELINERS=IO.readlines(rldir+'/rubylexer/test/oneliners.rb').map{|x| x.chomp}.grep(/\A\s*[^#\s]/).reverse
   STANZAS=IO.read(rldir+'/rubylexer/test/stanzas.rb').split("\n\n").grep(/./).reverse
   STANZAS.each{|stanza| stanza<<"\n" }
-  ILLEGAL_ONELINERS=IO.readlines(rldir+'/rubylexer/test/illegal_oneliners.rb').map{|x| x.chomp}.grep(/\A\s*[^#\s\n]/).reverse
+  ILLEGAL_ONELINERS=IO.readlines(rldir+'/rubylexer/test/illegal_oneliners.rb').map{|x| x.chomp}.grep(/\A\s*[^#\s]/).reverse
   ILLEGAL_STANZAS=IO.read(rldir+'/rubylexer/test/illegal_stanzas.rb').split("\n\n").grep(/./).reverse
 
   datadir=$:.find{|dir| File.exist? dir+'/../test/data/p.rb' }
