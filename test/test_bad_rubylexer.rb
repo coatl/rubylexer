a03d56200a9e43ebfe0606db7f4ef539c34e7b1a
Caleb Clausen <caleb@inforadical.net>
2011-09-30 07:09:44 -0700

    require rubygems in bad rubylexer test

--- test/test_bad_rubylexer.rb
+++ test/test_bad_rubylexer.rb
@@ -5,16 +5,12 @@ ruby='ruby' unless File.exist? ruby
 
 
 fail unless system(ruby,  "-e", <<END)
-   class X<RuntimeError; end;
    begin; 
      require '#{File.expand_path(File.join( File.dirname(__FILE__),'bad/ruby_lexer' ))}';  
+     require 'rubygems'
      require 'rubylexer';  
      rl=RubyLexer.new('eval','eval'); 
-     raise X;
-     rescue X; 
        fail if $the_wrong_rubylexer==1;
-     rescue Exception; fail;
-     else fail;
    end;
 END
 
